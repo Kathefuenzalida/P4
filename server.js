c6fe80a7a463eb57978d1c6068cd5e62e2a99cdc
KatherineFuenzalida <caterina.fdz@gmail.com>
2025-06-22 11:26:11 -0400

    change: "swagger.yaml fixed"

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const reservasRoutes = require('./routes/reservasRoutes');
-
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./docs/swagger.yaml');
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Middleware para leer JSON
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/reservas', reservasRoutes); // Rutas principales
 
 app.listen(PORT, () => {
